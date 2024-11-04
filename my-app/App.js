263a3d6cfe2f37156a0d384ef2eb47ec893809a6
ChouaiebAmine <chouaiebmedamine@gmail.com>
2024-11-04 14:31:12 +0100

    Adding some style

--- my-app/App.js
+++ my-app/App.js
@@ -5,6 +5,7 @@ import SignUp from "./SignUp"
 
 
 
+
 export default function App() {
 
   const [Accounts,setAccount] = useState([])
@@ -14,7 +15,9 @@ export default function App() {
   }
 
   return (
+
     <View style={styles.container}>
+      <Text style={styles.title} >Sign Up Form</Text>
       <SignUp AddAccount={addAccount}/>
       <StatusBar style="auto" />
     </View>
@@ -24,8 +27,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+
+  title:{
+    fontWeight:'bold',
+    fontSize:30,
+    padding:100,
+    color:'#FFF',
+    
+  }
 });
